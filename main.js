7c963fe3bb69b29b9fd7e0d4700895483662f428
matumartinez <mtmartinez2@uc.cl>
2026-04-25 17:21:44 -0400

    fix selector bug

--- main.js
+++ main.js
@@ -68,7 +68,7 @@ const PROVINCES = [
   { name: 'Curicó',                   region: 'Maule'              },
   { name: 'Linares',                  region: 'Maule'              },
   { name: 'Talca',                    region: 'Maule'              },
-  { name: 'Ñuble',                    region: 'Ñuble'              },
+  { name: 'Punilla',                    region: 'Ñuble'              },
   { name: 'Arauco',                   region: 'Bío-Bío'            },
   { name: 'Bio Bío',                  region: 'Bío-Bío'            },
   { name: 'Concepción',               region: 'Bío-Bío'            },
